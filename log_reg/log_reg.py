e9ab3c18cba169c8797bfee0203575f40dd3f1ad
Minh Le <minh.d.le27@gmail.com>
2016-05-21 18:28:52 -0700

    finished up sigmoid shit

--- log_reg/log_reg.py
+++ log_reg/log_reg.py
@@ -1,4 +1,5 @@
-include numpy as np
+import random
+import numpy as np
 import math
 def sigmoid(z):
     return (1/(1+math.e**(-z)))
@@ -15,8 +16,6 @@ def sigmoid(z):
         return 0
 def sigmoid_prime(z):
     return sigmoid(z)(1-sigmoid(z)) 
-    
-    
     if z >= 4:
         return 0
     elif z > 0:
@@ -30,28 +29,46 @@ def sigmoid_prime(z):
 
 class log_reg:
     def __init__(self,tin, tout):
-        self.weights
-        self.bias
         self.train_in = tin #array of input 
         self.train_out = tout #array of output
         self.n_train = len(tin)
-        
-
+        self.weights = np.random.rand(len(tin[0]))
+        self.bias = random.random()
         self.alpha = .03
 
     def compute(self, i_vec):
         return sigmoid(self.weights.dot(i_vec) + self.bias)
 
+    def cost(self):
+        c = 0
+        for n_tr in range(0, self.n_train):
+            y = self.train_out[n_tr]
+            guess = self.compute(self.train_in[n_tr])
+            c = c + (y * math.log(guess)) + ((1-y)+(math.log(1-guess)))
+        return c
     
-    
+    def train_set(self):
+        prev_cost = self.cost()
+        curr_cost = 0
+        thresh = 10 
+        limit = 1000
+        l_count = 0
+        while (prev_cost - curr_cost > thresh or l_count >= limit):
+            prev_cost = curr_cost
+            self.stoch_grad_desc()
+            curr_cost = self.cost()
+            print(curr_cost)
+            l_count += 1
+        print("training_done")
+
+
     def stoch_grad_desc(self):
         for n_tr in range(0, self.n_train):#for all training sets
-            funct = self.train_out[n_tr] - compute(self.train_in[n_tr])
-            for n_w in range(0,len(self.weights): #for all biases
-                self.weights[n_w] = self.weights[n_w] + 
-                    self.alpha * funct * self.train_in[n_tr][n_w] 
+            funct = self.train_out[n_tr] - self.compute(self.train_in[n_tr])
+            for n_w in range(0,len(self.weights)): #for all biases
+                self.weights[n_w] = self.weights[n_w] + self.alpha * funct * self.train_in[n_tr][n_w] 
         for n_tr in range(0, self.n_train):#for all training sets
-            funct = self.train_out[n_tr] - compute(self.train_in[n_tr])
+            funct = self.train_out[n_tr] - self.compute(self.train_in[n_tr])
             self.bias = self.bias + self.alpha * funct 
 
 
--- log_reg/main.py
+++ log_reg/main.py
@@ -1,5 +1,8 @@
-DATA_PATH = 
+import numpy as np
+import log_reg as log
 from mnist import MNIST
+
+DATA_PATH = './data'
 mndata = MNIST(DATA_PATH)
 mndata.load_training()
 mndata.load_testing()
@@ -8,11 +11,11 @@ mndata.load_testing()
 #getting only ones and zeros
 train_in =  []
 train_out = []
-
 for i in range(0, len(mndata.train_labels)):
-    if mndata.train_labels[i] == 1 or
-        mndata.train_labels[i] == 2:
+    if mndata.train_labels[i] == 0 or \
+        mndata.train_labels[i] == 1:
         train_in.append(np.array(mndata.train_images[i]))
         train_out.append(mndata.train_labels[i])
 
-
+neuron = log.log_reg(train_in, train_out)
+neuron.train_set()
